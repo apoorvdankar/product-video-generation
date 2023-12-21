07f1810fe213ae0007cf4ff99b0f975edbb9e68c
Apoorv Dankar <dankarapoorv@gmail.com>
2023-12-20 19:39:57 -0500

    adding gitignore and another exmple in the output

--- playground.py
+++
@@ -1,8 +0,0 @@
-import imageio
-from PIL import Image
-import numpy as np
-
-im1 = Image.open("gen_image.jpg")
-im2 = Image.open("gen1.jpg")
-
-imageio.mimsave("aq.mp4", [np.array(im1), np.array(im2)], format="mp4", fps=2)
